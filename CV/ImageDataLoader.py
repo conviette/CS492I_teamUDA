14b2805510d58e7ee8538ca5629e21103d790ab3
a-ntagonist <44609381+a-ntagonist@users.noreply.github.com>
2020-09-24 02:04:51 +0900

    pretrained patch

--- CV/ImageDataLoader.py
+++ CV/ImageDataLoader.py
@@ -15,8 +15,8 @@ class TransformTwice:
     def __call__(self, inp):
         out1 = self.transform(inp)
         out2 = self.transform(inp)
-        return out1, out2    
-    
+        return out1, out2
+
 class SimpleImageLoader(torch.utils.data.Dataset):
     def __init__(self, rootdir, split, ids=None, transform=None, loader=default_image_loader):
         if split == 'test':
@@ -28,12 +28,12 @@ class SimpleImageLoader(torch.utils.data.Dataset):
 
         imnames = []
         imclasses = []
-        
+
         with open(meta_file, 'r') as rf:
             for i, line in enumerate(rf):
                 if i == 0:
                     continue
-                instance_id, label, file_name = line.strip().split()        
+                instance_id, label, file_name = line.strip().split()
                 if int(label) == -1 and (split != 'unlabel' and split != 'test'):
                     continue
                 if int(label) != -1 and (split == 'unlabel' or split == 'test'):
@@ -50,11 +50,11 @@ class SimpleImageLoader(torch.utils.data.Dataset):
         self.split = split
         self.imnames = imnames
         self.imclasses = imclasses
-    
+
     def __getitem__(self, index):
         filename = self.imnames[index]
         img = self.loader(os.path.join(self.impath, filename))
-        
+
         if self.split == 'test':
             if self.transform is not None:
                 img = self.transform(img)
@@ -64,9 +64,9 @@ class SimpleImageLoader(torch.utils.data.Dataset):
                 img = self.transform(img)
             label = self.imclasses[index]
             return img, label
-        else:        
+        else:
             img1, img2 = self.TransformTwice(img)
             return img1, img2
-        
+
     def __len__(self):
         return len(self.imnames)
--- CV/models.py
+++ CV/models.py
@@ -33,23 +33,23 @@ def weights_init_kaiming(m):
     if classname.find('Conv') != -1:
         init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
         if m.bias is not None:
-            init.constant_(m.bias.data, 0.0)        
+            init.constant_(m.bias.data, 0.0)
     elif classname.find('Linear') != -1:
         init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
         if m.bias is not None:
             init.constant_(m.bias.data, 0.0)
     elif classname.find('BatchNorm1d') != -1:
         init.normal_(m.weight.data, 1.0, 0.02)
-        if m.bias is not None:        
+        if m.bias is not None:
             init.constant_(m.bias.data, 0.0)
     elif classname.find('BatchNorm2d') != -1:
         init.normal_(m.weight.data, 1.0, 0.02)
-        if m.bias is not None:        
+        if m.bias is not None:
             init.constant_(m.bias.data, 0.0)
     elif classname.find('BatchNorm') != -1:
         if m.affine is not None:
             init.constant_(m.weight.data, 1.0)
-            init.constant_(m.bias.data, 0.0)        
+            init.constant_(m.bias.data, 0.0)
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -75,19 +75,19 @@ def weights_init_classifier(m):
     classname = m.__class__.__name__
     if classname.find('Linear') != -1:
         init.normal_(m.weight.data, std=0.001)
-        init.constant_(m.bias.data, 0.0)     
-        
-######################################################################   
+        init.constant_(m.bias.data, 0.0)
+
+######################################################################
 class ClassBlock(nn.Module):
     def __init__(self, input_dim, class_num, dropout=True, relu=True, num_bottleneck=512): #512
         super(ClassBlock, self).__init__()
         add_block = []
-        add_block += [nn.Linear(input_dim, num_bottleneck)] 
+        add_block += [nn.Linear(input_dim, num_bottleneck)]
         add_block += [nn.BatchNorm1d(num_bottleneck)]
-        if relu: 
+        if relu:
             add_block += [nn.ReLU()]
-        if dropout: 
-            add_block += [nn.Dropout(p=0.5)] 
+        if dropout:
+            add_block += [nn.Dropout(p=0.5)]
         add_block = nn.Sequential(*add_block)
         add_block.apply(weights_init_kaiming)
         classifier = []
@@ -99,16 +99,16 @@ class ClassBlock(nn.Module):
     def forward(self, x):
         x = self.add_block(x)
         x = self.classifier(x)
-        return x    
-    
-######################################################################       
+        return x
+
+######################################################################
 # Define the ResNet18-based Model
-######################################################################     
+######################################################################
 class Res18_basic(nn.Module):
     def __init__(self, class_num):
         super(Res18_basic, self).__init__()
         fea_dim = 256
-        model_ft = models.resnet18(pretrained=True)
+        model_ft = models.resnet18(pretrained=False)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
         model_ft.fc = nn.Sequential()
         self.model = model_ft
@@ -116,7 +116,7 @@ class Res18_basic(nn.Module):
         self.fc_embed.apply(weights_init_classifier)
         self.classifier = ClassBlock(512, class_num)
         self.classifier.apply(weights_init_classifier)
-        
+
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
@@ -130,14 +130,14 @@ class Res18_basic(nn.Module):
         fea =  x.view(x.size(0), -1)
         embed_fea = self.fc_embed(fea)
         pred = self.classifier(fea)
-        return embed_fea, pred 
+        return embed_fea, pred
 
 # Define the ResNet18-based Model
 class Res18(nn.Module):
     def __init__(self, class_num):
         super(Res18, self).__init__()
         fea_dim = 256
-        model_ft = models.resnet18(pretrained=True)
+        model_ft = models.resnet18(pretrained=False)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
         model_ft.fc = nn.Sequential()
         self.model = model_ft
@@ -145,7 +145,7 @@ class Res18(nn.Module):
         self.fc_embed.apply(weights_init_classifier)
         self.classifier = ClassBlock(512, class_num)
         self.classifier.apply(weights_init_classifier)
-        
+
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
@@ -159,22 +159,22 @@ class Res18(nn.Module):
         fea =  x.view(x.size(0), -1)
         embed_fea = self.fc_embed(fea)
         pred = self.classifier(fea)
-        return embed_fea, pred 
-    
-        
+        return embed_fea, pred
+
+
 class Res50(nn.Module):
     def __init__(self, class_num):
         super(Res50, self).__init__()
-        fea_dim = 256        
-        model_ft = models.resnet50(pretrained=True)
+        fea_dim = 256
+        model_ft = models.resnet50(pretrained=False)
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        model_ft.fc = nn.Sequential()        
+        model_ft.fc = nn.Sequential()
         self.model = model_ft
         self.fc_embed = nn.Linear(2048, fea_dim)
         self.fc_embed.apply(weights_init_classifier)
         self.classifier = ClassBlock(2048, class_num)
-        self.classifier.apply(weights_init_classifier)        
-        
+        self.classifier.apply(weights_init_classifier)
+
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
@@ -188,21 +188,21 @@ class Res50(nn.Module):
         fea =  x.view(x.size(0), -1)
         embed_fea = self.fc_embed(fea)
         pred = self.classifier(fea)
-        return embed_fea, pred     
-        
+        return embed_fea, pred
+
 class Dense121(nn.Module):
     def __init__(self, class_num):
         super(Dense121, self).__init__()
-        fea_dim = 256        
-        model_ft = models.densenet121(pretrained=True)
+        fea_dim = 256
+        model_ft = models.densenet121(pretrained=False)
         model_ft.features.classifier = nn.Sequential()
         model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1,1))
         model_ft.features.fc_embed = nn.Linear(1024, fea_dim)
-        model_ft.features.fc_embed.apply(weights_init_classifier)  
+        model_ft.features.fc_embed.apply(weights_init_classifier)
         model_ft.classifier = ClassBlock(2048, class_num)
-        model_ft.classifier.apply(weights_init_classifier)  
+        model_ft.classifier.apply(weights_init_classifier)
         self.model = model_ft
-        
+
     def forward(self, x):
         x = self.model.features.conv0(x)
         x = self.model.features.norm0(x)
@@ -219,8 +219,5 @@ class Dense121(nn.Module):
         x = self.model.features.avgpool(x)
         fea =  x.view(x.size(0), -1)
         embed_fea = self.model.features.fc_embed(fea)
-        pred = self.model.classifier(fea)        
-        return embed_fea, pred              
-        
-
-            
+        pred = self.model.classifier(fea)
+        return embed_fea, pred
